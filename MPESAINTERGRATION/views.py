788b6f5306d405a40b7b678ddfc8eea353155275
sly18 <sylvianjoki37@gmail.com>
2023-04-25 17:31:16 +0300

    initial commit

---
+++ MPESAINTERGRATION/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django_daraja.mpesa import utils
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import View
+from django_daraja.mpesa.core import MpesaClient
+from decouple import config
+from datetime import datetime
+
+cl = MpesaClient()
+stk_push_callback_url = 'https://api.darajambili.com/express-payment'
+b2c_callback_url = 'https://api.darajambili.com/b2c/result'
+
+
+def oauth_success(request):
+    r = cl.auth_token()
+    return JsonResponse(r, safe=False)
+
+
+def Home(request):
+    if request.method == 'POST':
+        phone_number = request.POST.get('phone')
+        amount = request.POST.get('amount')
+        amount = int(amount)
+        account_reference = 'sylvia'
+        transaction_reference = 'STK push description'
+        callback_url = stk_push_callback_url
+        r = cl.stk_push(phone_number, amount, account_reference, transaction_reference, callback_url)
+        return JsonResponse(r.response_description, safe=False)
+
+
+
+    return render(request, 'index.html')
